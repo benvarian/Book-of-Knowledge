bf01016bcf3b6933496c4eb482009cc998a14498
benvarian <88700858+benvarian@users.noreply.github.com>
2023-10-05 21:57:21 +0800

    strip baby strip

--- mainpulate.py
+++ mainpulate.py
@@ -12,12 +12,12 @@ with open("datafile.json", "r") as outfile:
             pattern = rf"\({i+1}\)(.*?)\({i+2}\)"
             match = re.search(pattern, newstring)
             if match:
-                text_list.append(match.group(1))
+                text_list.append(match.group(1).strip())
         pattern = rf"\({len(num_matches)}\)(.*?)$"
         match = re.search(pattern, newstring)
         if match:
-            text_list.append(match.group(1))
+            text_list.append(match.group(1).strip())
         code[1]["Outcomes"] = text_list
-        
+
     with open("datafile_2.json", "w") as outfile:
         json.dump(file, outfile, indent=4)
