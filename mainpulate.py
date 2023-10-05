a10d0b5194def3732b7fe2313f8c15bd2f44a342
benvarian <88700858+benvarian@users.noreply.github.com>
2023-10-05 21:48:16 +0800

    i7 fix it felix

--- mainpulate.py
+++ mainpulate.py
@@ -8,4 +8,16 @@ with open("datafile.json", "r") as outfile:
         pattern = r"\(\d+\)"
         num_matches = re.findall(pattern, newstring)
         text_list = []
-        print(f"{newstring} {len(num_matches)} \n\n")
+        for i in range(len(num_matches) - 1):
+            pattern = rf"\({i+1}\)(.*?)\({i+2}\)"
+            match = re.search(pattern, newstring)
+            if match:
+                text_list.append(match.group(1))
+        pattern = rf"\({len(num_matches)}\)(.*?)$"
+        match = re.search(pattern, newstring)
+        if match:
+            text_list.append(match.group(1))
+        code[1]["Outcomes"] = text_list
+        
+    with open("datafile_2.json", "w") as outfile:
+        json.dump(file, outfile, indent=4)
