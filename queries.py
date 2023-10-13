74bb7cc4e3c77b137eb8f9ae13696d6a1ac45bbd
benvarian <88700858+benvarian@users.noreply.github.com>
2023-10-13 17:52:33 +0800

    query 1

--- queries.py
+++ queries.py
@@ -5,4 +5,25 @@ g = Graph()
 
 g.parse("handbook.ttl")
 
-print(g)
+# query 1
+# Find all units with more than 6 outcomes.
+query1 = """
+Prefix code: <https://uwa.handbook/code/> 
+Prefix rel: <https://uwa.handbook/relation/> 
+Prefix uwa: <https://uwa.handbook/> 
+Prefix xsd: <http://www.w3.org/2001/XMLSchema#> 
+
+SELECT ?Title ?Outcome (COUNT(?Outcome) AS ?count)
+WHERE {
+?p a uwa:unit.
+?p rel:Title ?Title.
+?p rel:Outcome ?Outcome.
+}
+GROUP BY ?Title
+HAVING (COUNT(?Outcome) > 6)
+"""
+for i in g.query(query1):
+    title = i["Title"]
+    count = i["count"]
+    print(f"{title}")
+
